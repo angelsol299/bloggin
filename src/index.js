ab65f38cf48aaaea5767a4b86f0133f902da0ed9
angelsol299 <angelsol299@hotmail.com>
2018-12-05 19:54:22 +0100

    main structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Main from "./Components/Main";
-import * as serviceWorker from "./serviceWorker";
-import "./styles/stylesheet.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './Components/Main'
+import './styles/stylesheet.css'
+import {BrowserRouter} from 'react-router-dom'
 
-ReactDOM.render(<Main />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'))

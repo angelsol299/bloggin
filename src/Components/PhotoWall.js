94148fb84515bac82894c5bed61a0bf4011ec977
angelsol299 <angelsol299@hotmail.com>
2018-12-06 21:42:09 +0100

    adding date

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -2,53 +2,57 @@ import React, { Component } from "react";
 import Photo from "./Photo";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Spinner from "./Spinner/Spinner";
 
 //anchor tag, href attribute
 
 class PhotoWall extends Component {
   render() {
     //nuevo
-
-    return (
-      <div>
-        <Link className="addIcon" to="/AddPhoto">
-          {" "}
-        </Link>
-        <br />
-        <div className="button-class">
-          <button
-            className="sort-button"
-            onClick={() =>
-              this.props.posts.sort(function(a, b) {
-                if (a < b) return -1;
-                else if (a > b) return 1;
-                return 0;
-              })
-            }
-          >
-            Latest
-          </button>
-          {"    "}
-          <button
-            className="sort-button"
-            onClick={() =>
-              this.props.posts.sort(function(a, b) {
-                return a < b ? -1 : 1;
-              })
-            }
-          >
-            By Subject
-          </button>
+    if (<Photo />) {
+      return (
+        <div>
+          <Link className="addIcon" to="/AddPhoto">
+            {" "}
+          </Link>
+          <br />
+          <div className="button-class">
+            <button
+              className="sort-button"
+              onClick={() =>
+                this.props.posts.sort(function(a, b) {
+                  if (a < b) return -1;
+                  else if (a > b) return 1;
+                  return 0;
+                })
+              }
+            >
+              Latest
+            </button>
+            {"    "}
+            <button
+              className="sort-button"
+              onClick={() =>
+                this.props.posts.sort(function(a, b) {
+                  return a < b ? -1 : 1;
+                })
+              }
+            >
+              By Subject
+            </button>
+          </div>
+          <br />
+          <br />
+          <div className="photoGrid">
+            {this.props.posts.map(post => (
+              <Photo post={post} {...this.props} />
+            ))}
+          </div>
         </div>
-        <br />
-        <br />
-        <div className="photoGrid">
-          {this.props.posts.map(post => (
-            <Photo post={post} {...this.props} />
-          ))}
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return <Spinner />;
+    }
   }
 }
 
